5cb53ec34261ca8ca6a6437e745218f22f664ee9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-27 21:34:57 +0000

    build based on 9a7df4b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BitSAD.jl-1","page":"Home","title":"BitSAD.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"BitSAD is a domain-specific language for bitstream computing.","category":"page"}]
+}
