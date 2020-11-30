28c0fed103a3ec6a70544dbf7e61261192e832c6
Kyle Daruwalla <daruwalla@wisc.edu>
2020-11-30 15:55:18 -0600

    Updated with proper handling for broadcasting

--- src/hardware/tracing.jl
+++ src/hardware/tracing.jl
@@ -27,21 +27,23 @@ Base.show(io::IO, ::MIME"text/plain", t::Trace) = print(io, strip("""
       $(map(i -> "$i\n  ", t)...)
     """))
 
+hassubtrace(c::TraceCall) = !isempty(c.subtrace)
+
 @kwdef mutable struct HardwareState
     current_trace::Trace = Trace()
     trace_stack::Vector{Trace} = [current_trace]
-    output_map::Dict{Tuple{Vararg{String}}, String} = Dict{Tuple{Vararg{String}}, String}()
+    output_map::Dict{Tuple{Bool, Vararg{String}}, String} = Dict{Tuple{Bool, Vararg{String}}, String}()
 end
 
-exists(state::HardwareState, f, args...) = haskey(state.output_map, (f, args...))
+exists(state::HardwareState, isbroadcast, f, args...) = haskey(state.output_map, (isbroadcast, f, args...))
 
-function set_output!(state::HardwareState, output, f, args...)
-    state.output_map[(f, args...)] = output
+function set_output!(state::HardwareState, output, isbroadcast, f, args...)
+    state.output_map[(isbroadcast, f, args...)] = output
 
     return state
 end
 
-get_output(state::HardwareState, f, args...) = state.output_map[(f, args...)]
+get_output(state::HardwareState, isbroadcast, f, args...) = state.output_map[(isbroadcast, f, args...)]
 
 Cassette.@context HardwareCtx
 
@@ -78,16 +80,17 @@ namify(x, ctx) = Cassette.hasmetadata(x, ctx) ? Cassette.metadata(x, ctx) : stri
 generate_output_name(f, args) =
     "net_$(f)_$(first(args))$(map(arg -> "_" * arg, Base.tail(args))...)"
 
-compress_name(name) = mapreduce(x -> first(x, 1), *, split(replace(name, "net_" => ""), "_"))
+compress_name(name) =
+    mapreduce(x -> startswith(x, ".") ? first(x, 2) : first(x, 1), *, split(replace(name, "net_" => ""), "_"))
 
-function select_output_name!(ctx::HardwareCtx, f, args...)
-    f_name = namify(f, ctx)
+function select_output_name!(ctx::HardwareCtx, f, args...; isbroadcast = false)
+    f_name = isbroadcast ? "." * namify(f, ctx) : namify(f, ctx)
     arg_names = namify.(args, Ref(ctx))
-    if exists(ctx.metadata, f_name, arg_names...)
-        return get_output(ctx.metadata, f_name, arg_names...)
+    if exists(ctx.metadata, isbroadcast, f_name, arg_names...)
+        return get_output(ctx.metadata, isbroadcast, f_name, arg_names...)
     else
         name = generate_output_name(compress_name(f_name), compress_name.(arg_names))
-        set_output!(ctx.metadata, name, f_name, arg_names...)
+        set_output!(ctx.metadata, name, isbroadcast, f_name, arg_names...)
 
         return name
     end
@@ -101,6 +104,7 @@ end
 
 function Cassette.posthook(ctx::HardwareCtx, output, f, args...)
     if !istraceprimitive(ctx, f, args...)
+        println("Popping $(namify(f, ctx))")
         removed_trace = pop!(ctx.metadata.trace_stack)
         ctx.metadata.current_trace = isempty(ctx.metadata.trace_stack) ? removed_trace :
                                                                          ctx.metadata.trace_stack[end]
@@ -108,31 +112,56 @@ function Cassette.posthook(ctx::HardwareCtx, output, f, args...)
     exit!(ctx, ctx.metadata.current_trace, output)
 end
 
-function Cassette.overdub(ctx::HardwareCtx, f, args...)
+function _overdub(ctx::HardwareCtx, f, args...; isbroadcast = false)
     unwrapped_f = Cassette.untag(f, ctx)
     unwrapped_args = map(arg -> Cassette.untag(arg, ctx), args)
     println("Calling $unwrapped_f($(unwrapped_args...))")
     if istraceprimitive(unwrapped_f, unwrapped_args...)
         result = unwrapped_f(unwrapped_args...)
-    else 
-        result = Cassette.untag(Cassette.recurse(ctx, f, args...), ctx)
-    end
+        name = select_output_name!(ctx, f, args...; isbroadcast = isbroadcast)
 
-    name = select_output_name!(ctx, f, args...)
-    return Cassette.tag(result, ctx, name)
+        return Cassette.tag(result, ctx, name)
+    else 
+        return Cassette.recurse(ctx, f, args...)
+    end
 end
+Cassette.overdub(ctx::HardwareCtx, f, args...) = _overdub(ctx, f, args...)
+
+
+## Broadcasting hooks
+
+_materialize(ctx, x) = Cassette.tag(Base.Broadcast.materialize(Cassette.untag(x, ctx)), ctx, namify(x, ctx))
 
 function Cassette.prehook(ctx::HardwareCtx, ::typeof(Base.broadcasted), f, args...)
-    subtrace = enter!(ctx, ctx.metadata.current_trace, f, args...; isbroadcast = true)
-    !istraceprimitive(ctx, f, args...) && push!(ctx.metadata.trace_stack, subtrace)
+    materialized_args = map(arg -> _materialize(ctx, arg), args)
+    subtrace = enter!(ctx, ctx.metadata.current_trace, f, materialized_args...; isbroadcast = true)
+    !istraceprimitive(ctx, f, materialized_args...) && push!(ctx.metadata.trace_stack, subtrace)
     ctx.metadata.current_trace = ctx.metadata.trace_stack[end]
 end
 
-Cassette.posthook(ctx::HardwareCtx, output, ::typeof(Base.broadcasted), f, args...) =
-    Cassette.posthook(ctx, output, f, args...)
+function Cassette.posthook(ctx::HardwareCtx, output, ::typeof(Base.broadcasted), f, args...)
+    materialized_args = map(arg -> _materialize(ctx, arg), args)
+    materialized_output = _materialize(ctx, output)
+    Cassette.posthook(ctx, materialized_output, f, materialized_args...)
+end
 
-Cassette.overdub(ctx::HardwareCtx, ::typeof(Base.materialize), args...) =
-    Cassette.fallback(ctx, Base.materialize, args...)
+function Cassette.overdub(ctx::HardwareCtx, ::typeof(Base.broadcasted), f, args...)
+    arg_names = namify.(args, Ref(ctx))
+    unwrapped_args = map(arg -> Cassette.untag(arg, ctx), args)
+    element_args = map((arg, name) -> Cassette.tag(first(arg), ctx, name), unwrapped_args, arg_names)
+    element_result = _overdub(ctx, f, element_args...; isbroadcast = true)
+    result = Base.broadcasted(Cassette.untag(f, ctx), unwrapped_args...)
+
+    return Cassette.tag(result, ctx, namify(element_result, ctx))
+end
+
+## Broadcasting primitives
+#   don't recurse these calls
+
+Cassette.prehook(ctx::HardwareCtx, ::typeof(Base.materialize), args...) = nothing
+Cassette.posthook(ctx::HardwareCtx, output, ::typeof(Base.materialize), args...) = nothing
+Cassette.overdub(ctx::HardwareCtx, ::typeof(Base.materialize), arg) =
+    Cassette.tag(Cassette.fallback(ctx, Base.materialize, arg), ctx, namify(arg, ctx))
 
 macro trace_primitive(f, args...)
     return quote
@@ -140,4 +169,35 @@ macro trace_primitive(f, args...)
         #     Cassette.fallback($(esc(f)), $(esc.(args)...))
         HW.istraceprimitive(::typeof($(esc(f))), $(esc.(args)...)) = true
     end
+end
+
+symbol_name(x) = QuoteNode(:($x))
+
+function _generate(name, ex)
+    if @capture(ex, f_(args__))
+        tagged_args = map(arg -> Symbol(symbol_name(arg), :_tag), args)
+        tagged_stmts = map((dest, arg) -> @q($dest = Cassette.tag($(esc(arg)), ctx, string($(symbol_name(arg))))),
+                           tagged_args, args)
+
+        @q begin
+            ctx = Cassette.enabletagging(HardwareCtx(metadata = HardwareState()), $(esc(f)))
+            $(tagged_stmts...)
+            Cassette.@overdub ctx $(esc(f))($(tagged_args...))
+
+            m = Module(name = $(symbol_name(name)))
+            extracttrace!(m, ctx.metadata.current_trace)
+
+            m
+        end
+    else
+        error("@generate expects a function call (e.g. f(x, y))")
+    end
+end
+
+macro generate(ex)
+    return _generate(:top, ex)
+end
+
+macro generate(name, ex)
+    return _generate(name, ex)
 end
