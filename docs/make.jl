5db319b6f6ca2c6ef965a05eaf192965476a652e
Kyle Daruwalla <daruwalla@wisc.edu>
2021-09-03 20:19:40 -0500

    Updated docs/make.jl to Publish

--- docs/make.jl
+++ docs/make.jl
@@ -1,35 +1,15 @@
-using Documenter, BitSAD
+using Publish
+using Artifacts, LazyArtifacts
+using BitSAD
 
-makedocs(;
-    modules=[BitSAD],
-    format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-        "Introduction" => [
-            "Getting started" => "getting-started.md",
-            "Stochastic Bitstream Walkthrough" => "sbitstream-example.md",
-            "Deterministic Bitstream Walkthrough" => "dbitstream-example.md",
-        ],
-        "Types" => [
-            "Abstract Bitstreams" => "bitstream.md",
-            "Stochastic Bitstreams" => "sbitstream.md",
-            "Deterministic Bitstreams" => "dbitstream.md",
-        ],
-        "Hardware Generation" => [
-            "Getting started" => "hardware-generation.md",
-            "Internals" => "hardware-internals.md",
-        ],
-        "Customizing BitSAD" => [
-            "Custom hardware generation" => "custom-hardware.md",
-            "Custom `SBitstream` operators" => "custom-soperators.md",
-        ],
-    ],
-    repo="https://github.com/UW-PHARM/BitSAD.jl/blob/{commit}{path}#L{line}",
-    sitename="BitSAD.jl",
-    authors="Kyle Daruwalla, University of Wisconsin-Madison PHARM Group",
-    assets=String[],
-)
+# override default theme
+Publish.Themes.default() = artifact"flux-theme"
 
-deploydocs(;
-    repo="github.com/UW-PHARM/BitSAD.jl",
-)
+p = Publish.Project(BitSAD)
+
+# needed to prevent error when overwriting
+rm("dev", recursive = true, force = true)
+rm(p.env["version"], recursive = true, force = true)
+
+# build documentation
+deploy(Metalhead; root = "/BitSAD.jl", force = true, label = "dev")
