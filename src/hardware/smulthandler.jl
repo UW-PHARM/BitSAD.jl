6bbbabe59732fbbaddea9a68367ed8128e898334
Kyle Daruwalla <daruwalla@wisc.edu>
2022-02-02 11:23:26 -0600

    temp save

--- src/hardware/smulthandler.jl
+++ src/hardware/smulthandler.jl
@@ -7,8 +7,8 @@ init_state(::SMultHandler) = (id = 0,)
 
 gethandler(broadcasted::Bool,
            ::Type{typeof(*)},
-           ::Type{<:AbstractArray{<:SBitstream}},
-           ::Type{<:AbstractArray{<:SBitstream}}) =
+           ::Type{<:AbstractArray{<:SBitstream, 2}},
+           ::Type{<:AbstractArray{<:SBitstream, 2}}) =
     broadcasted ? SMultHandler() : SMatMultHandler()
 init_state(::SMatMultHandler) = (id = 0,)
 
@@ -22,85 +22,24 @@ function (handler::SMultHandler)(buffer, netlist, state, inputs, outputs)
                                          netsize(inputs[1]), netsize(inputs[2]))
     outsize = netsize(outputs[1])
 
-    # add internal nets to netlist
-    push!(netlist, Net(name = "mult$(state.id)_out_pp", size = outsize))
-    push!(netlist, Net(name = "mult$(state.id)_out_pm", size = outsize))
-    push!(netlist, Net(name = "mult$(state.id)_out_mp", size = outsize))
-    push!(netlist, Net(name = "mult$(state.id)_out_mm", size = outsize))
-    push!(netlist, Net(name = "mult$(state.id)_out_11", size = outsize))
-    push!(netlist, Net(name = "mult$(state.id)_out_12", size = outsize))
-    push!(netlist, Net(name = "mult$(state.id)_out_13", size = outsize))
-    push!(netlist, Net(name = "mult$(state.id)_out_14", size = outsize))
-
     # add output net to netlist
     setsigned!(netlist, outputs[1], true)
 
     write(buffer, """
         $stdcomment
         // BEGIN mult$(state.id)
-        assign mult$(state.id)_out_pp = $(lname("_p")) & $(rname("_p"))
-        assign mult$(state.id)_out_pm = $(lname("_p")) & $(rname("_m"))
-        assign mult$(state.id)_out_mp = $(lname("_m")) & $(rname("_p"))
-        assign mult$(state.id)_out_mm = $(lname("_m")) & $(rname("_m"))
-        stoch_sat_sub_mat #(
+        stoch_signed_elem_mult_mat #(
                 .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mult$(state.id)_11 (
+                .NUM_COL($(outsize[2]))
+            ) mult$(state.id) (
                 .CLK(CLK),
                 .nRST(nRST),
-                .A(mult$(state.id)_out_pp),
-                .B(mult$(state.id)_out_pm),
-                .Y(mult$(state.id)_out_11)
-            );
-        stoch_sat_sub_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mult$(state.id)_12 (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mult$(state.id)_out_pm),
-                .B(mult$(state.id)_out_pp),
-                .Y(mult$(state.id)_out_12)
-            );
-        stoch_sat_sub_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mult$(state.id)_13 (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mult$(state.id)_out_mp),
-                .B(mult$(state.id)_out_mm),
-                .Y(mult$(state.id)_out_13)
-            );
-        stoch_sat_sub_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mult$(state.id)_14 (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mult$(state.id)_out_mm),
-                .B(mult$(state.id)_out_mp),
-                .Y(mult$(state.id)_out_14)
-            );
-        stoch_add_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mult$(state.id)_p (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mult$(state.id)_out_11),
-                .B(mult$(state.id)_out_14),
-                .Y($(name(outputs[1]))_p)
-            );
-        stoch_add_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mult$(state.id)_m (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mult$(state.id)_out_12),
-                .B(mult$(state.id)_out_13),
-                .Y($(name(outputs[1]))_m)
+                .A_p($(lname("_p"))),
+                .A_m($(lname("_m"))),
+                .B_p($(rname("_p"))),
+                .B_m($(rname("_m"))),
+                .Y_p($(name(outputs[1]))_p),
+                .Y_m($(name(outputs[1]))_m)
             );
         // END mult$(state.id)
         \n""")
@@ -121,122 +60,22 @@ function (handler::SMatMultHandler)(buffer, netlist, state, inputs, outputs)
     # add output net to netlist
     setsigned!(netlist, outputs[1], true)
 
-    # add internal nets to netlist
-    push!(netlist, Net(name = "mmult$(state.id)_out_pp", size = outsize))
-    push!(netlist, Net(name = "mmult$(state.id)_out_pm", size = outsize))
-    push!(netlist, Net(name = "mmult$(state.id)_out_mp", size = outsize))
-    push!(netlist, Net(name = "mmult$(state.id)_out_mm", size = outsize))
-    push!(netlist, Net(name = "mmult$(state.id)_out_11", size = outsize))
-    push!(netlist, Net(name = "mmult$(state.id)_out_12", size = outsize))
-    push!(netlist, Net(name = "mmult$(state.id)_out_13", size = outsize))
-    push!(netlist, Net(name = "mmult$(state.id)_out_14", size = outsize))
-
     write(buffer, """
         $stdcomment
         // BEGIN mmult$(state.id)
-        stoch_matrix_mult #(
+        stoch_signed_matrix_mult #(
                 .NUM_ROWS($m),
                 .NUM_MID($n),
                 .NUM_COLS($p)
-            ) mmult$(state.id)_pp (
+            ) mmult$(state.id) (
                 .CLK(CLK),
                 .nRST(nRST),
-                .A($(name(inputs[1]))_p),
-                .B($(name(inputs[2]))_p),
-                .Y(mmult$(state.id)_out_pp)
-            );
-        stoch_matrix_mult #(
-                .NUM_ROWS($m),
-                .NUM_MID($n),
-                .NUM_COLS($p)
-            ) mmult$(state.id)_pm (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A($(name(inputs[1]))_p),
-                .B($(name(inputs[2]))_m),
-                .Y(mmult$(state.id)_out_pm)
-            );
-        stoch_matrix_mult #(
-                .NUM_ROWS($m),
-                .NUM_MID($n),
-                .NUM_COLS($p)
-            ) mmult$(state.id)_mp (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A($(name(inputs[1]))_m),
-                .B($(name(inputs[2]))_p),
-                .Y(mmult$(state.id)_out_mp)
-            );
-        stoch_matrix_mult #(
-                .NUM_ROWS($m),
-                .NUM_MID($n),
-                .NUM_COLS($p)
-            ) mmult$(state.id)_mm (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A($(name(inputs[1]))_m),
-                .B($(name(inputs[2]))_m),
-                .Y(mmult$(state.id)_out_mm)
-            );
-        stoch_sat_sub_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mmult$(state.id)_11 (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mmult$(state.id)_out_pp),
-                .B(mmult$(state.id)_out_pm),
-                .Y(mmult$(state.id)_out_11)
-            );
-        stoch_sat_sub_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mmult$(state.id)_12 (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mmult$(state.id)_out_pm),
-                .B(mmult$(state.id)_out_pp),
-                .Y(mmult$(state.id)_out_12)
-            );
-        stoch_sat_sub_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mmult$(state.id)_13 (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mmult$(state.id)_out_mp),
-                .B(mmult$(state.id)_out_mm),
-                .Y(mmult$(state.id)_out_13)
-            );
-        stoch_sat_sub_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mmult$(state.id)_14 (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mmult$(state.id)_out_mm),
-                .B(mmult$(state.id)_out_mp),
-                .Y(mmult$(state.id)_out_14)
-            );
-        stoch_add_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mmult$(state.id)_p (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mmult$(state.id)_out_11),
-                .B(mmult$(state.id)_out_14),
-                .Y($(name(outputs[1]))_p)
-            );
-        stoch_add_mat #(
-                .NUM_ROWS($(outsize[1])),
-                .NUM_COLS($(outsize[2]))
-            ) mmult$(state.id)_m (
-                .CLK(CLK),
-                .nRST(nRST),
-                .A(mmult$(state.id)_out_12),
-                .B(mmult$(state.id)_out_13),
-                .Y($(name(outputs[1]))_m)
+                .A_p($(name(inputs[1]))_p),
+                .A_m($(name(inputs[1]))_m),
+                .B_p($(name(inputs[2]))_p),
+                .B_m($(name(inputs[2]))_m),
+                .Y_p($(name(outputs[1]))_p),
+                .Y_m($(name(outputs[1]))_m)
             );
         // END mmult$(state.id)
         \n""")
--- src/hardware/utils.jl
+++ src/hardware/utils.jl
@@ -22,12 +22,38 @@ handle_broadcast_name(lhs::String, rhs::String, lsize, rsize) =
                        suffix -> "{$(lsize[1] * lsize[2]){$(rhs)$suffix}}") :
     (suffix -> "$(lhs)$suffix", suffix -> "$(rhs)$suffix")
 
+function write_for_loop(buffer, index_name, sz_name, stride, write_body, padding = 0)
+    maybesz = tryparse(Int, sz_name)
+    if isnothing(maybesz) || (maybesz < 10)
+        write(buffer, repeat(" ", padding))
+        write(buffer, "for ($index_name = 0; $index_name < $sz_name; $index_name = $index_name + $stride) begin : $(index_name)_gen\n")
+        write_body(buffer, index_name, padding + 1)
+        write(buffer, repeat(" ", padding))
+        write(buffer, "end")
+    else
+        index_name_blk = index_name * "_blk"
+        write(buffer, repeat(" ", padding))
+        write(buffer, "for ($index_name_blk = 0; $index_name_blk < $maybesz / 10; $index_name_blk = $index_name_blk + 1) begin : $(index_name_blk)_gen\n")
+        write(buffer, repeat(" ", padding + 1))
+        write(buffer, "for ($index_name = $index_name_blk * 10; $index_name < 10; $index_name = $index_name + $stride) begin : $(index_name)_gen\n")
+        write_body(buffer, index_name, padding + 2)
+        write(buffer, repeat(" ", padding + 1))
+        write(buffer, "end")
+        write(buffer, repeat(" ", padding))
+        write(buffer, "end")
+    end
+end
+
 function write_bcast_instantiation(buffer, prefix, outsize, body)
     write(buffer, """
         genvar $(join(ntuple(i -> "$(prefix)_i_$i", length(outsize)), ", "));
 
         generate
         """)
+
+    padding = 0
+    for sz in outsize
+        
     for (i, sz) in enumerate(outsize)
         write(buffer, repeat(" ", i - 1) * "for ($(prefix)_i_$i = 0; $(prefix)_i_$i < $sz; $(prefix)_i_$i = $(prefix)_i_$i + 1) begin : $(prefix)_gen_$i\n")
     end
