93d9fb8529c18be16c55b9852e8d7722c0b62f14
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-15 13:22:51 -0500

    Add `isinf` and `isnan`

--- src/types/sbitstream.jl
+++ src/types/sbitstream.jl
@@ -55,6 +55,9 @@ Base.promote_rule(::Type{SBitstream{T}}, ::Type{S}) where {T, S<:Number} =
 Base.typemin(::Type{T}) where {T<:SBitstream} = zero(T)
 Base.typemax(::Type{T}) where {T<:SBitstream} = one(T)
 
+Base.isinf(::SBitstream) = false
+Base.isnan(::SBitstream) = false
+
 Base.isless(x::SBitstream, y::SBitstream) = Base.isless(float(x), float(y))
 Base.:(==)(x::SBitstream, y::SBitstream) = (float(x) == float(y))
 Base.hash(x::SBitstream, h::UInt) = hash(float(x), h)
